3d6527b0f102b35dfed41dc10813d588a066911e
mbusson <mbusson@users.noreply.github.com>
2016-05-05 18:53:49 +0200

    Added files via upload

--- index.php
+++ index.php
@@ -13,30 +13,39 @@
 	    <div class="loader-section section-left"></div>
 	    <div class="loader-section section-right"></div>
 	</div>
-	<div id="wrapper">
-		<div id="spanner"></div>
-		<div id="centerer">
-			<div class="box" class="hover">
-				<a href="#"></a><img src="images/template.png"></a>
-			</div>
-			<div class="box" class="hover">
-				<img src="images/template.png">
-			</div>
-			<div class="box" class="hover">
-				<img src="images/template.png">
-			</div><br>
+	<div id="centerer">
+		<div class="box" class="hover-scale" id='polpo'>
+			<img src="images/polpo.jpg">
 		</div>
-		<div id="centerer2">
-			<div class="box" class="hover">
-				<img src="images/template.png">
-			</div>
-			<div class="box" class="hover">
-				<img src="images/template.png">
-			</div>
-			<div class="box" class="hover">
-				<img src="images/template.png">
-			</div>
+		<div class="box" class="hover-scale" id='medusa'>
+			<img src="images/medusa.jpg">
 		</div>
+		<div class="box" class="hover-scale" id='sirena'>
+			<img src="images/sirena.jpg">
+		</div>
+	</div>
+	<div id="centerer2">
+		<div class="box" class="hover-scale" id='ippo'>
+			<img src="images/ippocampo.jpg">
+		</div>
+		<div class="box" class="hover-scale" id='lepisma'>
+			<img src="images/lepisma.jpg">
+		</div>
+		<div class="box" class="hover-scale" id='pesce'>
+			<img src="images/pesce.jpg">
+		</div><br>
+	</div>
+	<div id="home">
+		<a href="#menu" id="toggle"><span></span></a>
+		<div id="menu">
+			<ul>
+				<li><a href="#home"><i class="fa fa-envelope-o" aria-hidden="true"></i></a></li>
+				<li><a href="../"><i class="fa fa-home" aria-hidden="true"></i></a></li>
+			</ul>
+		</div>
+	</div>
+	<div id="multi-wrap">
+
 	</div>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.4/TweenMax.min.js"></script>
--- style.js
+++ style.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 // --- GSAP ---
 	function startFinalAnimation(){
     	var fa = new TimelineLite();
-    	fa.from("#wrapper", 2, {backgroundColor:"rgba(152, 49, 240, 0.75)", ease:Back.easeIn});
+    	fa.from("#wrapper", 2, {backgroundColor:"rgba(0,0,0, 0.75)", ease:Back.easeIn});
 		fa.staggerFrom(".box", 0.25, {opacity:0, x:-250, y:-150, ease:Back.easeIn}, 0.2);
 		fa.set(".box", {clearProps:"all"});
 	};
@@ -20,19 +20,78 @@ $(document).ready(function(){
 	var hvalue1 = $('#wrapper .box').width();
 	$(this).css({'height': hvalue1+'px'});
 
-	var centerer = $('#centerer'), centerer2 = $('#centerer2'), body = $(window), 
-		ch = centerer.outerHeight(true),
-		c2h = centerer2.outerHeight(true),
-		takenspace = ch + c2h,
-		page = body.height(),
-		available = page - takenspace,
-		span = (available / 2);
-		console.log(ch);
-		console.log(c2h);
-		console.log(takenspace);
-		console.log(page);
-		console.log(available);
-		console.log(span);
-	$('#spanner').css({'height': span+'px'});
+// --- MENU - pureJS ---
+
+	var iconToggle = document.getElementById('toggle');
+
+	function hasClass(elem, className) {
+		return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+	}
+	function addClass(elem, className) {
+	    if (!hasClass(elem, className)) {
+	    	elem.className += ' ' + className;
+	    }
+	}
+	function removeClass(elem, className) {
+		var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
+		if (hasClass(elem, className)) {
+	        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+	            newClass = newClass.replace(' ' + className + ' ', ' ');
+	        }
+	        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+	    }
+	}
+	function toggleClass(elem, className) {
+		var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
+	    if (hasClass(elem, className)) {
+	        while (newClass.indexOf(" " + className + " ") >= 0 ) {
+	            newClass = newClass.replace( " " + className + " " , " " );
+	        }
+	        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+	    } else {
+	        elem.className += ' ' + className;
+	    }
+	}
+	iconToggle.onclick = function() {
+	   toggleClass(this, 'on');
+	   return false;
+	}
+
+// --- CALLS ---
+var polpo = '<img src="images/polpo.jpg">',
+	medusa = '<img src="images/medusa.jpg">',
+	sirena = '<img src="images/sirena.jpg">',
+	ippo = '<img src="images/ippocampo.jpg">',
+	lepisma = '<img src="images/lepisma.jpg">',
+	pesce = '<img src="images/pesce.jpg">';
+
+	$('#multi-wrap').click(function() {
+		$(this).empty();
+		$(this).css('visibility', 'hidden')
+	});
+	$('#polpo').click(function() {
+		$('#multi-wrap').css('visibility', 'visible');
+		$('#multi-wrap').append(polpo);
+	});
+	$('#medusa').click(function() {
+		$('#multi-wrap').css('visibility', 'visible');
+		$('#multi-wrap').append(medusa);
+	});
+	$('#sirena').click(function() {
+		$('#multi-wrap').css('visibility', 'visible');
+		$('#multi-wrap').append(sirena);
+	});
+	$('#ippo').click(function() {
+		$('#multi-wrap').css('visibility', 'visible');
+		$('#multi-wrap').append(ippo);
+	});
+	$('#lepisma').click(function() {
+		$('#multi-wrap').css('visibility', 'visible');
+		$('#multi-wrap').append(lepisma);
+	});
+	$('#pesce').click(function() {
+		$('#multi-wrap').css('visibility', 'visible');
+		$('#multi-wrap').append(pesce);
+	});
 
 });
